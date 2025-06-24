c30e90e71e38025914d4a3f26595aca270408c45
AngieCaiza27 <acaiza0477@uta.edu.ec>
2025-06-24 12:09:31 -0500

    Credenciales de la nueva base hosteada

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,26 +1,26 @@
-import pkg from 'pg';          
+import pkg from 'pg';
 const { Client } = pkg;
 
- class DataBase{
+class DataBase {
   static instancia;
-  constructor (){
+  constructor() {
     if (DataBase.instancia) {
       return DataBase.instancia;
     }
 
-   this.client = new Client({
-      user: 'admin',
-      host: 'dpg-d0j12bd6ubrc73cko57g-a.oregon-postgres.render.com',
-      database: 'gestionpruebas',
-      password: 'cTbyF9p3fcC4Yo7xpFNzcLwtvK6TboTH',
-      port: 5432, 
-       ssl: {
+    this.client = new Client({
+      user: 'root',
+      host: 'dpg-d1bio0je5dus73em5fng-a.oregon-postgres.render.com',
+      database: 'gestionseguros',
+      password: 'qT0zrHI3ph9rIrx2RzvmezVr0qtp4Dp9',
+      port: 5432,
+      ssl: {
         rejectUnauthorized: false // ⚠ Importante para Render
       }
     });
 
-    this.#connect(); 
-    DataBase.instancia=this;
+    this.#connect();
+    DataBase.instancia = this;
   }
   async #connect() {
     try {
@@ -30,10 +30,10 @@ const { Client } = pkg;
       console.error('❌ Error al conectar a PostgreSQL:', error);
     }
   }
- 
- getConexion() {
-  return this.client;
-}
+
+  getConexion() {
+    return this.client;
+  }
 }
 export { DataBase };
 
