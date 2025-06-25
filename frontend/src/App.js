f1c4cb1e199557a5577c3f4517b3a0a19b87578d
AngieCaiza27 <acaiza0477@uta.edu.ec>
2025-06-25 13:57:42 -0500

    Pruebas Fronted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import AppRoutes from './AppRoutes';
-import Login from './views/Login';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./views/Login.jsx";
+import VentanaAdmin from "./views/usuarios/VentanaAdmin.jsx";
+import VentanaAgente from "./views/usuarios/VentanaAgente.jsx";
+import VentanaCliente from "./views/usuarios/ventanaCliente.jsx";
+import ValidarContratacionSeguro from "./views/validaciones/ValidarContratacionSeguro";
+import ValidarEmail from "./views/validaciones/ValidarEmail";
+import ValidarAgente from "./views/validaciones/ValidarAgente";
 
 function App() {
-  const user = sessionStorage.getItem('user');
-
-  if (user) {
-    return <AppRoutes />;
-  }
-  return <Login />;
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/admin" element={<VentanaAdmin />} />
+          <Route path="/agente" element={<VentanaAgente />} />
+          <Route path="/cliente" element={<VentanaCliente />} />
+          <Route path="/validacionContratacion/:id" element={<ValidarContratacionSeguro />} />
+          <Route path="/validacionEmail/:id" element={<ValidarEmail />} />
+          <Route path="/validacionAgente/:id" element={<ValidarAgente />} />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
 
 export default App;
