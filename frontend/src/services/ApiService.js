aad698bb07ded5b434eb405f5738ff2bb5e7a612
Dennis-Chimborazo <dacdalexch@gmail.com>
2025-06-25 12:30:59 -0500

    Se crea gestion  de revision de los reeembolsos para agentes y admin

--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -61,8 +61,7 @@ class ApiService {
     return Array.isArray(response.data) ? response.data : [];
   }
 
-    static async getNull(getApi, id, navigate) {
-    console.log(`${apiUrl}${getApi}?id=${id}`)
+  static async getNull(getApi, id, navigate) {
     const response = await axios.get(`${apiUrl}${getApi}?id=${id}`, {
       headers: {
         "Content-Type": "application/json",
@@ -119,21 +118,43 @@ class ApiService {
 
     return response.data;
   }
-static async getArchivo(getApi, id, tipo, navigate) {
-  const tokenInfo = JSON.parse(localStorage.getItem("login"));
-  const token = tokenInfo ? tokenInfo.token : "";
+  
+  static async getArchivo(getApi, id, tipo, navigate) {
+    const tokenInfo = JSON.parse(localStorage.getItem("login"));
+    const token = tokenInfo ? tokenInfo.token : "";
 
-  const url = `${apiUrl}${getApi}/${id}?tipo=${tipo}`;
+    const url = `${apiUrl}${getApi}/${id}?tipo=${tipo}`;
 
-  const response = await axios.get(url, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
+    const response = await axios.get(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  return response.data.url;
-}
+    return response.data.url;
+  }
+
+  static async getArchivoSearch(getApi, nombreBase, navigate) {
+    const tokenInfo = JSON.parse(localStorage.getItem("login"));
+    const token = tokenInfo ? tokenInfo.token : "";
+    const url = `${apiUrl}${getApi}?nombre=${encodeURIComponent(nombreBase)}`;
+    try {
+      const response = await axios.get(url, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data.url; // Devuelve la URL del PDF
+
+    } catch (error) {
+      console.error("🔴 Error al obtener archivo PDF:", error);
+      if (navigate) navigate("/error");
+      throw error;
+    }
+  }
+
 
 }
 
